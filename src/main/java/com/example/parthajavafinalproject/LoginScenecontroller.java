2593db8ff480f23c164f850a8959f8dcf6a30e53
partha3411 <2022067@iub.edu.bd>
2025-03-14 14:01:45 +0600

    startedwithloginscene

--- src/main/java/com/example/parthajavafinalproject/LoginScenecontroller.java
+++ src/main/java/com/example/parthajavafinalproject/LoginScenecontroller.java
@@ -1,7 +1,31 @@
 package com.example.parthajavafinalproject;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class LoginScenecontroller
 {
     @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private ComboBox userTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField passwordTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void waiterLoginButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void customerLoginButtonOnAction(ActionEvent actionEvent) {
+    }
+}
