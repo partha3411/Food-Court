80fc4e9967b22ade3f4f99a07d48ea1914d53ea0
partha3411 <2022067@iub.edu.bd>
2025-03-24 15:32:00 +0600

    Scenes added  with controllers chef prepares special dish chef reviews customer critics

---
+++ src/main/java/com/example/parthajavafinalproject/ChefPreparesAspecialDishSceneController.java
@@ -0,0 +1,39 @@
+package com.example.parthajavafinalproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ChefPreparesAspecialDishSceneController
+{
+    @javafx.fxml.FXML
+    private TextField phoneNumberTextField;
+    @javafx.fxml.FXML
+    private TableColumn orderDetailsCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn phonenumberCol;
+    @javafx.fxml.FXML
+    private TableView tableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/parthajavafinalproject/ChefReviewsCustomerCriticsSceneController.java
@@ -0,0 +1,27 @@
+package com.example.parthajavafinalproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ChefReviewsCustomerCriticsSceneController
+{
+    @javafx.fxml.FXML
+    private TableColumn criticsCol;
+    @javafx.fxml.FXML
+    private TableColumn phonenumberCol;
+    @javafx.fxml.FXML
+    private TableView tableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loadButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+}
