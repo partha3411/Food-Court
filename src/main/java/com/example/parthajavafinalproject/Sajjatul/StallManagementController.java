9729c6c49013cff535a2558d2d53153bc404f5f3
Shishir707 <mirshr8@gmail.com>
2025-04-15 01:46:29 +0600

    design stall management scene

---
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/StallManagementController.java
@@ -0,0 +1,53 @@
+package com.example.parthajavafinalproject.Sajjatul;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+public class StallManagementController {
+
+    @FXML
+    private TableColumn<?, ?> billingDateColoumn;
+
+    @FXML
+    private ComboBox<?> durationComboBox;
+
+    @FXML
+    private Text label;
+
+    @FXML
+    private TableColumn<?, ?> nextBillingDateColoumn;
+
+    @FXML
+    private ComboBox<?> paymentComboBox;
+
+    @FXML
+    private ComboBox<?> stallComboBox;
+
+    @FXML
+    private TableColumn<?, ?> stallNameColoumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColoumn;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @FXML
+    void confirmButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void downloadReceiptButton(ActionEvent event) {
+
+    }
+
+}
