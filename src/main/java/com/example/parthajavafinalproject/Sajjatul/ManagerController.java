55da24dba83bf14e49519801ac8dbebd3bc485df
Shishir707 <mirshr8@gmail.com>
2025-04-15 01:05:09 +0600

    design scene for manager

--- src/main/java/com/example/parthajavafinalproject/Sajjatul/ManagerController.java
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/ManagerController.java
@@ -1,8 +1,65 @@
 package com.example.parthajavafinalproject.Sajjatul;
 
-public class ManagerController
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class ManagerController {
+
+    @FXML
+    private Button stallManagement;
+
+    @FXML
+    private VBox vbox;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
+
+    @FXML
+    void feedbackRatings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void liveChat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCloseTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void realTimeSales(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void staffDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stallManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stockManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateApp(ActionEvent event) {
+
+    }
+
 }
