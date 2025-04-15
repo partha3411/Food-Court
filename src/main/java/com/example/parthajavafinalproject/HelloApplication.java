4032516a42f95dc146dd6b9e865b07d4c48baebb
Shishir707 <mirshr8@gmail.com>
2025-04-15 10:00:11 +0600

    Added branch and add previous

--- src/main/java/com/example/parthajavafinalproject/HelloApplication.java
+++ src/main/java/com/example/parthajavafinalproject/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("manager.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("stallManagement.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("FoodCourt!");
         stage.setScene(scene);
         stage.show();
