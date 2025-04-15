624b0db6ccc01f0bc5faa38b0c6712c91bbe92a9
Shishir707 <mirshr8@gmail.com>
2025-04-15 20:51:09 +0600

    added cashier scene

---
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/CashierController.java
@@ -0,0 +1,62 @@
+package com.example.parthajavafinalproject.Sajjatul;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class CashierController {
+
+    @FXML
+    private VBox vbox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+
+    @FXML
+    void bestSellItemButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dailyNeedsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void digitalReceiptButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void discountOfferButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void paymentButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salesReportButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void staffBillButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void utilityBillButton(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/com/example/parthajavafinalproject/Sajjatul/ManagerController.java
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/ManagerController.java
@@ -8,58 +8,90 @@ import javafx.scene.layout.VBox;
 public class ManagerController {
 
     @FXML
-    private Button stallManagement;
-
-    @FXML
     private VBox vbox;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
-    @FXML
+    @Deprecated
     void feedbackRatings(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void liveChat(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void openCloseTime(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void realTimeSales(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void signOut(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void staffDetails(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void stallManagement(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void stockManagement(ActionEvent event) {
 
     }
 
-    @FXML
+    @Deprecated
     void updateApp(ActionEvent event) {
 
     }
 
+    @FXML
+    public void salesReportButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void utilityBillButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void dailyNeedsButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void paymentButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void bestSellItemButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void discountOfferButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void staffBillButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void digitalReceiptButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void signOutButton(ActionEvent actionEvent) {
+    }
 }
