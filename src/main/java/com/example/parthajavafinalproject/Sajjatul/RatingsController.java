5ad12c4d3fe9ab86dc3f6f2dfa7b7e1fa79ac7c4
Shishir707 <mirshr8@gmail.com>
2025-04-15 19:57:18 +0600

    design rating & feedback scene

---
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/RatingsController.java
@@ -0,0 +1,63 @@
+package com.example.parthajavafinalproject.Sajjatul;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class RatingsController {
+
+    @FXML
+    private TableColumn<?, ?> feedbackCol;
+
+    @FXML
+    private TextField feedbackTF;
+
+    @FXML
+    private ComboBox<?> fooNameCB;
+
+    @FXML
+    private TableColumn<?, ?> foodNameCol;
+
+    @FXML
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private TextField nameTF;
+
+    @FXML
+    private ComboBox<?> ratingCB;
+
+    @FXML
+    private TableColumn<?, ?> ratingCol;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @FXML
+    void backMenuButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filterButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ratingDilterCB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitButton(ActionEvent event) {
+
+    }
+
+}
