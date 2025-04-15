6e90a69b46b84d3b469bb61b0ab0572cb0046252
Shishir707 <mirshr8@gmail.com>
2025-04-16 02:29:17 +0600

    implement real time sell

--- src/main/java/com/example/parthajavafinalproject/Sajjatul/RealTimeSalesController.java
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/RealTimeSalesController.java
@@ -7,18 +7,48 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class RealTimeSalesController {
 
+    @FXML
+    private ComboBox<?> itemAddCB;
+
+    @FXML
+    private ComboBox<?> itemOrderCB;
+
+    @FXML
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private TextField quantityAddTF;
+
+    @FXML
+    private TextField quantityOrderTF;
+
+    @FXML
+    private TableColumn<?, ?> remainCol;
+
+    @FXML
+    private TableView<?> tableView;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @FXML
-    void backMenuButton(ActionEvent event)  throws IOException {
+    void addItemButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backMenuButton(ActionEvent event) throws IOException {
         Parent root = null ;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sajjatul/manager.fxml"));
         root = fxmlLoader.load();
@@ -27,6 +57,11 @@ public class RealTimeSalesController {
         stage.setScene(scene);
         stage.setTitle("Manager Dashboard");
         stage.show();
+
+    }
+
+    @FXML
+    void orderButton(ActionEvent event) {
 
     }
 
