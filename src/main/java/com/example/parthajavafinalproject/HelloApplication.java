3e4998b57bb1a23a9eece4b247e986fc875ff163
sojib <2220117@iub.edu.bd>
2025-04-15 23:31:01 +0600

    FirstSojib

--- src/main/java/com/example/parthajavafinalproject/HelloApplication.java
+++ src/main/java/com/example/parthajavafinalproject/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("stallManagement.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("FoodCourt!");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,7 @@ module com.example.parthajavafinalproject {
     opens com.example.parthajavafinalproject to javafx.fxml;
     opens com.example.parthajavafinalproject.Sajjatul to javafx.fxml;
     exports com.example.parthajavafinalproject;
+
+    opens com.example.parthajavafinalproject.Sojib to javafx.fxml;
+    exports com.example.parthajavafinalproject.Sojib;
 }
