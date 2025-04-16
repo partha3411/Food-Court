b1fbbe8576ac8c00cb67121894325644a37a5618
Shishir707 <mirshr8@gmail.com>
2025-04-16 10:44:02 +0600

    implement stock management

--- src/main/java/com/example/parthajavafinalproject/Sajjatul/StallManagement.java
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/StallManagement.java
@@ -1,4 +1,63 @@
 package com.example.parthajavafinalproject.Sajjatul;
 
+import java.time.LocalDate;
+
 public class StallManagement {
+    private String stallName;
+    private LocalDate billingDate;
+    private LocalDate nextBillingDate;
+    private String status;
+
+    public StallManagement(LocalDate billingDate, LocalDate nextBillingDate, String stallName, String status) {
+        this.billingDate = billingDate;
+        this.nextBillingDate = nextBillingDate;
+        this.stallName = stallName;
+        this.status = status;
+    }
+
+    public StallManagement(String stall, LocalDate today, LocalDate nextBillingDate, String status) {
+
+    }
+
+    public LocalDate getBillingDate() {
+        return billingDate;
+    }
+
+    public void setBillingDate(LocalDate billingDate) {
+        this.billingDate = billingDate;
+    }
+
+    public LocalDate getNextBillingDate() {
+        return nextBillingDate;
+    }
+
+    public void setNextBillingDate(LocalDate nextBillingDate) {
+        this.nextBillingDate = nextBillingDate;
+    }
+
+    public String getStallName() {
+        return stallName;
+    }
+
+    public void setStallName(String stallName) {
+        this.stallName = stallName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "StallManagement{" +
+                "billingDate=" + billingDate +
+                ", stallName='" + stallName + '\'' +
+                ", nextBillingDate=" + nextBillingDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/parthajavafinalproject/Sajjatul/StallManagementController.java
+++ src/main/java/com/example/parthajavafinalproject/Sajjatul/StallManagementController.java
@@ -10,51 +10,105 @@ import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.time.LocalDate;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class StallManagementController {
 
     @FXML
-    private TableColumn<?, ?> billingDateColoumn;
+    private TableColumn<StallManagement, LocalDate> billingDateColoumn;
 
     @FXML
-    private ComboBox<?> durationComboBox;
+    private ComboBox<String> durationComboBox;
 
     @FXML
     private Text label;
 
     @FXML
-    private TableColumn<?, ?> nextBillingDateColoumn;
+    private TableColumn<StallManagement, LocalDate> nextBillingDateColoumn;
 
     @FXML
-    private ComboBox<?> paymentComboBox;
+    private ComboBox<String> paymentComboBox;
 
     @FXML
-    private ComboBox<?> stallComboBox;
+    private ComboBox<String> stallComboBox;
 
     @FXML
-    private TableColumn<?, ?> stallNameColoumn;
+    private TableColumn<StallManagement, String> stallNameColoumn;
 
     @FXML
-    private TableColumn<?, ?> statusColoumn;
+    private TableColumn<StallManagement, String> statusColoumn;
 
     @FXML
-    private TableView<?> tableView;
+    private TableView<StallManagement> tableView;
 
     @javafx.fxml.FXML
     public void initialize() {
+        stallComboBox.getItems().addAll("Food Stall 1","Food Stall 2","Food Stall 3");
+        durationComboBox.getItems().addAll("1 Month","2 Month","3 Month","6 Month ");
+        paymentComboBox.getItems().addAll("Cash","Bkash","Nagad","Rocket","Upay","Card");
+
+        stallNameColoumn.setCellValueFactory(new PropertyValueFactory<>("stallName"));
+        billingDateColoumn.setCellValueFactory(new PropertyValueFactory<>("billingDate"));
+        nextBillingDateColoumn.setCellValueFactory(new PropertyValueFactory<>("nextBillingDate"));
+        statusColoumn.setCellValueFactory(new PropertyValueFactory<>("status"));
     }
+
+    ArrayList<StallManagement> stallManagements=new ArrayList<>();
 
     @FXML
     void confirmButton(ActionEvent event) {
+        String stall = stallComboBox.getValue();
+        String duration = durationComboBox.getValue();
+        String payment = paymentComboBox.getValue();
 
+        if (stall==null || duration==null || payment==null) {
+            label.setText("Please select all fields");
+            return;
+        }
+        LocalDate today = LocalDate.now();
+        LocalDate nextBillingDate = switch (duration){
+            case "1 Month"-> today.plusMonths(1);
+            case "2 Month"-> today.plusMonths(2);
+            case "3 Month"-> today.plusMonths(3);
+            case "6 Month"-> today.plusMonths(6);
+            default -> today.plusMonths(1);
+        };
+
+        String status = payment.equals("Cash") ? "Paid Offline" : "Paid Online";
+
+        StallManagement info = new StallManagement(stall, today, nextBillingDate, status);
+        label.setText("Billing confirmed for " + stall);
+        tableView.getItems().add(info);
     }
 
     @FXML
     void downloadReceiptButton(ActionEvent event) {
+        String stall = stallComboBox.getValue();
+        String duration = durationComboBox.getValue();
+        String payment = paymentComboBox.getValue();
+        tableView.getItems().clear();
+        LocalDate today = LocalDate.now();
+        LocalDate nextBillingDate = switch (duration){
+            case "1 Month"-> today.plusMonths(1);
+            case "2 Month"-> today.plusMonths(2);
+            case "3 Month"-> today.plusMonths(3);
+            case "6 Month"-> today.plusMonths(6);
+            default -> today.plusMonths(1);
+        };
+
+        String status = payment.equals("Cash") ? "Paid Offline" : "Paid Online";
+
+        StallManagement info = new StallManagement(stall, today, nextBillingDate, status);
+        label.setText("Dowloaded");
+        tableView.getItems().add(info);
+
 
     }
 
