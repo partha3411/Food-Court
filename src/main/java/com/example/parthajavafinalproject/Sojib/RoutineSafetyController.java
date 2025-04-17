636e7342a979bd1516233f786e7507ce64bbc9d9
sojib <2220117@iub.edu.bd>
2025-04-17 23:01:47 +0600

    .

--- src/main/java/com/example/parthajavafinalproject/Sojib/RoutineSafetyController.java
+++ src/main/java/com/example/parthajavafinalproject/Sojib/RoutineSafetyController.java
@@ -1,10 +1,18 @@
 package com.example.parthajavafinalproject.Sojib;
 
+import com.example.parthajavafinalproject.HelloApplication;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class RoutineSafetyController {
@@ -68,5 +76,17 @@ public class RoutineSafetyController {
         alert.setHeaderText(null);
         alert.setContentText("Please fill in all fields and select at least one description.");
         alert.showAndWait();
+    }
+    @FXML
+    void backButton(ActionEvent event)  throws IOException {
+        Parent root = null ;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(""));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Health Dashboard");
+        stage.show();
+
     }
 }
