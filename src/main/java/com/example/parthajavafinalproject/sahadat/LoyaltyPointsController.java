4a089726ea88271b44bfafc03ea1cac48da30494
Admin <2221634@iub.edu.bd>
2025-04-17 20:11:45 +0600

    Commit8

---
+++ src/main/java/com/example/parthajavafinalproject/sahadat/LoyaltyPointsController.java
@@ -0,0 +1,27 @@
+package com.example.parthajavafinalproject.sahadat;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class LoyaltyPointsController {
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+    @javafx.fxml.FXML
+    private Label label3;
+
+    @javafx.fxml.FXML
+    public void voucher2Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void voucher1Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void voucher3Button(ActionEvent actionEvent) {
+    }
+}
