7f5380447c1a18fb5203b9489beb920dc0b674e0
sojib <2220117@iub.edu.bd>
2025-04-18 01:32:17 +0600

    .

---
+++ src/main/java/com/example/parthajavafinalproject/Sojib/EmergencyTrainingController.java
@@ -0,0 +1,33 @@
+package com.example.parthajavafinalproject.Sojib;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class EmergencyTrainingController {
+    @javafx.fxml.FXML
+    private RadioButton EE;
+    @javafx.fxml.FXML
+    private RadioButton firee;
+    @javafx.fxml.FXML
+    private RadioButton FAK;
+    @javafx.fxml.FXML
+    private TableColumn nameTable;
+    @javafx.fxml.FXML
+    private Label QArea;
+    @javafx.fxml.FXML
+    private TableColumn chooseTbale;
+    @javafx.fxml.FXML
+    private TableView EmergencyTraining;
+    @javafx.fxml.FXML
+    private TableColumn doneTable;
+    @javafx.fxml.FXML
+    private TextField Ename;
+
+    @javafx.fxml.FXML
+    public void submit(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
