32d32e8c2fc8f873609c4b8ab164b3786bf1f549
Admin <2221634@iub.edu.bd>
2025-04-18 14:58:45 +0600

    Commit14

---
+++ src/main/java/com/example/parthajavafinalproject/sahadat/DeliveryIssueController.java
@@ -0,0 +1,21 @@
+package com.example.parthajavafinalproject.sahadat;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class DeliveryIssueController {
+    @javafx.fxml.FXML
+    private Button handleSubmit;
+    @javafx.fxml.FXML
+    private TextArea ta1;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private ComboBox customerCb;
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+}
