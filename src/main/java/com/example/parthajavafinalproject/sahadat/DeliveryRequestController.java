7226560d014719848b871f422953437f2cbdee3b
Admin <2221634@iub.edu.bd>
2025-04-18 11:11:31 +0600

    Commit9

---
+++ src/main/java/com/example/parthajavafinalproject/sahadat/DeliveryRequestController.java
@@ -0,0 +1,23 @@
+package com.example.parthajavafinalproject.sahadat;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class DeliveryRequestController {
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+    @javafx.fxml.FXML
+    private Label label3;
+
+    @javafx.fxml.FXML
+    public void handleDecline(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAccept(ActionEvent actionEvent) {
+    }
+}
