aeaf0b0106dea92e4571c2b2d6ea828eaa10cf46
Admin <2221634@iub.edu.bd>
2025-04-18 11:42:37 +0600

    Commit10

--- src/main/java/com/example/parthajavafinalproject/sahadat/FoodDeliveryPartnerDashboardController.java
+++ src/main/java/com/example/parthajavafinalproject/sahadat/FoodDeliveryPartnerDashboardController.java
@@ -1,4 +1,35 @@
 package com.example.parthajavafinalproject.sahadat;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class FoodDeliveryPartnerDashboardController {
+    @javafx.fxml.FXML
+    private TextArea ta1;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TextField tx1;
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+    @javafx.fxml.FXML
+    private Label label3;
+    @javafx.fxml.FXML
+    private TextField tx2;
+
+    @javafx.fxml.FXML
+    public void handleAvailability(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRefresh(ActionEvent actionEvent) {
+    }
 }
