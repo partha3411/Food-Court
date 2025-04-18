a98e185943cde2aa2aa41c0c1c3df92036a1ca7b
Admin <2221634@iub.edu.bd>
2025-04-18 18:20:38 +0600

    Commit17

---
+++ src/main/java/com/example/parthajavafinalproject/sahadat/EarningsViewController.java
@@ -0,0 +1,27 @@
+package com.example.parthajavafinalproject.sahadat;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+
+public class EarningsViewController {
+    @javafx.fxml.FXML
+    private TableColumn orderIdColumn;
+    @javafx.fxml.FXML
+    private TableView earningsTable;
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private Button withdrawButton;
+    @javafx.fxml.FXML
+    private Label balanceLabel;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
+    public void withDrawButton(ActionEvent actionEvent) {
+    }
+}
