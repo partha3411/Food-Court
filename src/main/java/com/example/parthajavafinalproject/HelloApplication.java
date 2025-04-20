385006dd95946fa096515e51aa800606bd58e01b
Admin <2221634@iub.edu.bd>
2025-04-20 22:02:53 +0600

    Commit26

--- src/main/java/com/example/parthajavafinalproject/HelloApplication.java
+++ src/main/java/com/example/parthajavafinalproject/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/customerDashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sajjatul/manager.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("FoodCourt!");
         stage.setScene(scene);
