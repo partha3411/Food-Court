ab428aecb6c3858288a6fe48e09c991b23e07a58
Shishir707 <mirshr8@gmail.com>
2025-04-21 23:02:52 +0600

    Solve version JDK

--- src/main/java/com/example/parthajavafinalproject/HelloApplication.java
+++ src/main/java/com/example/parthajavafinalproject/HelloApplication.java
@@ -11,17 +11,13 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-        // Set the stage for the SceneSwitcher
         SceneSwitcher.setStage(stage);
-
-        // Set the initial scene, e.g., your dashboard
         try {
             SceneSwitcher.switchTo("loginScene");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Set the title and show the stage
         stage.setTitle("Food Court Management System");
         stage.show();
     }
