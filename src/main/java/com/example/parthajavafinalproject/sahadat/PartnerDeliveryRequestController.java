e23c919d5d741f53f44c9e08ebb8903758e20e2c
Admin <2221634@iub.edu.bd>
2025-04-21 20:08:22 +0600

    Commit32

--- src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDeliveryRequestController.java
+++ src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDeliveryRequestController.java
@@ -1,12 +1,12 @@
 package com.example.parthajavafinalproject.sahadat;
 
-
 import com.example.parthajavafinalproject.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,16 +16,41 @@ public class PartnerDeliveryRequestController {
 
     @javafx.fxml.FXML
     private AnchorPane TF2;
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+    @javafx.fxml.FXML
+    private Label label3;
+    @javafx.fxml.FXML
+    private Label label4;
+
+
+    @javafx.fxml.FXML
+    public void initialize() {
+        label1.setText("Restaurant: Pizza Palace");
+        label2.setText("Pickup Location: 123 Main Street");
+        label3.setText("Earning: 750TK");
+        label4.setText(""); // Start with empty status
+    }
 
     @javafx.fxml.FXML
     public void partnerDashboard(ActionEvent event) throws IOException {
-        Parent root = null;
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerDashboard.fxml"));
-        root = fxmlLoader.load();
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("sahadat/partnerDashboard.fxml"));
         Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.setTitle("Back");
         stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void declineButton(ActionEvent actionEvent) {
+        label4.setText("Order Rejected");
+    }
+
+    @javafx.fxml.FXML
+    public void acceptButton(ActionEvent actionEvent) {
+        label4.setText("Order Accepted");
     }
 }
