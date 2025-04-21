f9aed46eef78422d9e2a747edf0bc9d9666524f0
Admin <2221634@iub.edu.bd>
2025-04-21 08:02:04 +0600

    Commit27

--- src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDashboardController.java
+++ src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDashboardController.java
@@ -1,60 +1,123 @@
 package com.example.parthajavafinalproject.sahadat;
 
+import com.example.parthajavafinalproject.HelloApplication;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PartnerDashboardController {
 
-    @Deprecated
-    public void handleAvailability(ActionEvent actionEvent) {
+    @javafx.fxml.FXML
+    public void partnerAvailabilityStatus(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerAvailabilityStatus.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Availability Status");
+        stage.show();
+
     }
 
     @javafx.fxml.FXML
-    public void handleLogout(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void handleRefresh(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void programsButton(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void ratebutton(ActionEvent actionEvent) {
+    public void partnerRatingView(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerRatingView.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Rating");
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void trackButton(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void accetButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void updateButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void deliveryOrder(ActionEvent actionEvent) {
+    public void partnerPickupOrder(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerPickupOrder.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Pickup Order");
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void pickupButton(ActionEvent actionEvent) {
+    public void partnerDeliveryIssue(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerDeliveryIssue.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Delivery Issue");
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void deliveryIssues(ActionEvent actionEvent) {
+    public void partnerEarningsView(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerEarningView.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Earning");
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void rateButton(ActionEvent actionEvent) {
+    public void partnerIncentivePrograms(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sahadat/partnerIncentivePrograms.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Participate Incentive Program");
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void acceptButton(ActionEvent actionEvent) {
+    public void handleLogout(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginScene.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void partnerDeliveryRequestView(ActionEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("partnerDeliveryRequestView.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root) ;
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Delivery Request");
+        stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void partnerDeliverOrder(ActionEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("partnerDeliverOrder.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root) ;
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Deliver Order");
+        stage.show();
     }
 }
--- src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDeliveryRequestController.java
+++ src/main/java/com/example/parthajavafinalproject/sahadat/PartnerDeliveryRequestController.java
@@ -1,31 +1,13 @@
 package com.example.parthajavafinalproject.sahadat;
 
-import javafx.event.ActionEvent;
+
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class PartnerDeliveryRequestController {
+
     @javafx.fxml.FXML
-    private Label label;
-    @javafx.fxml.FXML
-    private Label label1;
-    @javafx.fxml.FXML
-    private Label label2;
-    @javafx.fxml.FXML
-    private Label label3;
-    @javafx.fxml.FXML
-    private TextField TF3;
+    private Label acDeLB;
     @javafx.fxml.FXML
     private AnchorPane TF2;
-    @javafx.fxml.FXML
-    private TextField TF1;
-
-    @javafx.fxml.FXML
-    public void handleDecline(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void handleAccept(ActionEvent actionEvent) {
-    }
 }
